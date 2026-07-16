706e6e30e544b0458854fb48ce7bff7ca7cf94d6
medvika <info@medvika.co.in>
2026-07-16 19:39:06 +0530

    Add searchMedicine function for medicine search

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+function searchMedicine() {
+    let medicine = document.getElementById("medicineSearch").value;
 
+    if (medicine === "") {
+        alert("Please enter medicine name");
+    } else {
+        alert("Searching for: " + medicine);
+    }
+}
